ba1dc869f6758ecec93d448137c566a08affdc73
Léa Narzis <78718413+lean-apple@users.noreply.github.com>
2026-02-25 13:24:44 +0100

    fix: fix migration (#11)

--- db/migrations/1772008896268-Data.js
+++ db/migrations/1772008896268-Data.js
@@ -2,8 +2,8 @@ module.exports = class Data1772008896268 {
     name = 'Data1772008896268'
 
     async up(db) {
-        await db.query(`ALTER TABLE "assertion_added" ADD "log_index" integer NOT NULL`)
-        await db.query(`ALTER TABLE "assertion_removed" ADD "log_index" integer NOT NULL`)
+        await db.query(`ALTER TABLE "assertion_added" ADD "log_index" integer NOT NULL DEFAULT 0`)
+        await db.query(`ALTER TABLE "assertion_removed" ADD "log_index" integer NOT NULL DEFAULT 0`)
     }
 
     async down(db) {
