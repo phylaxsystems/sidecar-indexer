72018220a58b7b6044839350e3eb98dcca5a40b8
Lea Na <78718413+lean-apple@users.noreply.github.com>
2026-03-11 16:47:53 +0100

    fix: fix conversion

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { TypeormDatabase } from "@subsquid/typeorm-store";
 import { events } from "./abi/StateOracle.js";
+import { bytes } from "./model/generated/marshal.js";
 import { AssertionAdded } from "./model/generated/assertionAdded.model.js";
 import { AssertionRemoved } from "./model/generated/assertionRemoved.model.js";
 import { processor } from "./processor.js";
@@ -24,8 +25,8 @@ processor.run(db, async (ctx) => {
             assertionId: decoded.assertionId,
             activationBlock: decoded.activationBlock,
             daVerifier: decoded.daVerifier,
-            metadata: decoded.metadata,
-            proof: decoded.proof,
+            metadata: bytes.fromJSON(decoded.metadata),
+            proof: bytes.fromJSON(decoded.proof),
           }),
         );
         ctx.log.info(
