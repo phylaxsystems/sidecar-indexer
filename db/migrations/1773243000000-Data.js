b4221914bcc9cf6f5b667378e9d0b714a75a3229
Lea Na <78718413+lean-apple@users.noreply.github.com>
2026-03-11 16:48:20 +0100

    feat: add migration file with default

---
+++ db/migrations/1773243000000-Data.js
@@ -0,0 +1,18 @@
+module.exports = class Data1773243000000 {
+    name = 'Data1773243000000'
+
+    async up(db) {
+        await db.query(`ALTER TABLE "assertion_added" ADD "da_verifier" text NOT NULL DEFAULT '0x0000000000000000000000000000000000000000'`)
+        await db.query(`ALTER TABLE "assertion_added" ADD "metadata" bytea NOT NULL DEFAULT '\\x'`)
+        await db.query(`ALTER TABLE "assertion_added" ADD "proof" bytea NOT NULL DEFAULT '\\x'`)
+        await db.query(`ALTER TABLE "assertion_added" ALTER COLUMN "da_verifier" DROP DEFAULT`)
+        await db.query(`ALTER TABLE "assertion_added" ALTER COLUMN "metadata" DROP DEFAULT`)
+        await db.query(`ALTER TABLE "assertion_added" ALTER COLUMN "proof" DROP DEFAULT`)
+    }
+
+    async down(db) {
+        await db.query(`ALTER TABLE "assertion_added" DROP COLUMN "proof"`)
+        await db.query(`ALTER TABLE "assertion_added" DROP COLUMN "metadata"`)
+        await db.query(`ALTER TABLE "assertion_added" DROP COLUMN "da_verifier"`)
+    }
+}
